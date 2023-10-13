3566dc8c5efd3199f24e267ef435d0bc6f786646
CodeWithSomesh <codewithsomesh@gmail.com>
2023-10-13 10:42:12 +0800

    Updated Navbar and File Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/HomePage'
 
 function App() {
 
 
   return (
-    <div className='bg-red-500'>Test</div>
+    <Routes> 
+      <Route path='/' element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Route>
+
+    </Routes>
   )
 }
 

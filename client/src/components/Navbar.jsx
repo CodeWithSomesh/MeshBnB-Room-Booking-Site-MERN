2cce77c5963bd66da6a27c2493537159c9ea3f61
CodeWithSomesh <codewithsomesh@gmail.com>
2023-10-15 22:58:24 +0800

    Edited Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,14 +4,14 @@ const Header = () => {
   return (
     <div className='flex justify-between'>
 
-        <div className="logo-icon flex gap-2 items-center">
+        <Link to={'/'} className="logo-icon flex gap-2 items-center">
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
             </svg>
 
             <span className='font-bold text-xl' >MeshBnB</span>
-        </div>
+        </Link>
 
         <Link to={'/login'} className="hamburger-user-icon flex">
 

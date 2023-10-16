9510f97dd1d28585f2540cccaaf5fcf4809ce4d1
CodeWithSomesh <codewithsomesh@gmail.com>
2023-10-16 20:12:05 +0800

    Editing Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-between'>
 
-        <Link to={'/'} className="logo-icon flex gap-2 items-center">
+        <Link to={'/'} className="logo-icon flex gap-2 items-center p-2 border border-2 border-gray-400 rounded-lg">
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />

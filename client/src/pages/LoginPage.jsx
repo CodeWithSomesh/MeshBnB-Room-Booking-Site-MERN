0cda0e73df8ba16189845603e43c4dd6ad065e0c
CodeWithSomesh <codewithsomesh@gmail.com>
2023-10-20 16:21:20 +0800

    Done with Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -5,10 +5,14 @@ const LoginPage = () => {
     <div className="mt-4 grow flex flex-col items-center justify-center mb-32">
       <h1 className="text-4xl text-center mb-4">Login</h1>
       <form className="max-w-md mx-auto">
-        <input type="email" placeholder="your@email.com" />
-        <input type="password" placeholder="password" />
-        <button className="primary">Login</button>
-        <Link to={'/register'}></Link>
+        <input type="email" placeholder="Your@email.com" />
+        <input type="password" placeholder="Password" />
+        <button className="primary text-lg font-bold mt-4">Login</button>
+
+        <div className="text-center text-gray-500 mt-2">
+          Don't have an account yet? <Link to={'/register'} className="underline text-black">Register now</Link>
+        </div>
+        
       </form>
     </div>
   )

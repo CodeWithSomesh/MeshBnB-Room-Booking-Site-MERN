8b0e32bd3b76b614c12bcf0ad7d5ddbd6f58e0b0
CodeWithSomesh <codewithsomesh@gmail.com>
2023-10-24 21:29:59 +0800

    Connected to Mongo Atlas Database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-//mongo atlas password 
+//mongo atlas password = ZsTUIpsJArf9lZl4
 
 const express = require('express'); 
-// Allow Frontend Server communicate with Backend Server while ensuring security
-const cors = require('cors'); 
+const cors = require('cors');  // Allow Frontend Server communicate with Backend Server while ensuring security
+const mongoose = require('mongoose');
+const User = require('./models/User'); //Importing UserModels from "User.js"
+require('dotenv').config(); //Require this package to read 'enc' files succesfully 
 const app = express();
 
 app.use(express.json()); //Using JSON Parser
@@ -12,12 +14,21 @@ app.use(cors({
     origin: 'http://localhost:5173',
 }))
 
+//  Connecting to Mongo Atlas 
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
     res.json('test ok')
 })
 
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
+    User.create({
+        name, 
+        email, 
+        password,
+    });
+
     res.json({name, email, password});
 });
 
